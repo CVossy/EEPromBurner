fe5902c10ea4f6f86090eea517319ad5440367eb
Cvossy <sadazachiel@yahoo.de>
2014-05-09 22:49:16 +0200

    Added C# tool

---
+++ EEPROM_burner_vossy/EEPROM_burner_vossy/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace EEPROM_burner_vossy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cbxCom = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbCodeView = new System.Windows.Forms.RichTextBox();
+            this.btnHexRead = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_burn = new System.Windows.Forms.Button();
+            this.tbxHexFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 238);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(456, 160);
+            this.listBox1.TabIndex = 0;
+            // 
+            // cbxCom
+            // 
+            this.cbxCom.FormattingEnabled = true;
+            this.cbxCom.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "COM11",
+            "COM12",
+            "COM13",
+            "COM14",
+            "COM15"});
+            this.cbxCom.Location = new System.Drawing.Point(12, 28);
+            this.cbxCom.Name = "cbxCom";
+            this.cbxCom.Size = new System.Drawing.Size(121, 21);
+            this.cbxCom.TabIndex = 1;
+            this.cbxCom.SelectedIndexChanged += new System.EventHandler(this.cbxCom_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Comport:";
+            // 
+            // rtbCodeView
+            // 
+            this.rtbCodeView.Location = new System.Drawing.Point(12, 87);
+            this.rtbCodeView.Name = "rtbCodeView";
+            this.rtbCodeView.Size = new System.Drawing.Size(454, 130);
+            this.rtbCodeView.TabIndex = 3;
+            this.rtbCodeView.Text = "";
+            // 
+            // btnHexRead
+            // 
+            this.btnHexRead.Location = new System.Drawing.Point(355, 26);
+            this.btnHexRead.Name = "btnHexRead";
+            this.btnHexRead.Size = new System.Drawing.Size(111, 23);
+            this.btnHexRead.TabIndex = 4;
+            this.btnHexRead.Text = "Code einlesen";
+            this.btnHexRead.UseVisualStyleBackColor = true;
+            this.btnHexRead.Click += new System.EventHandler(this.btnHexRead_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_burn
+            // 
+            this.btn_burn.Location = new System.Drawing.Point(12, 415);
+            this.btn_burn.Name = "btn_burn";
+            this.btn_burn.Size = new System.Drawing.Size(75, 23);
+            this.btn_burn.TabIndex = 5;
+            this.btn_burn.Text = "Burn!!!";
+            this.btn_burn.UseVisualStyleBackColor = true;
+            this.btn_burn.Click += new System.EventHandler(this.btn_burn_Click);
+            // 
+            // tbxHexFile
+            // 
+            this.tbxHexFile.Location = new System.Drawing.Point(139, 28);
+            this.tbxHexFile.Name = "tbxHexFile";
+            this.tbxHexFile.Size = new System.Drawing.Size(210, 20);
+            this.tbxHexFile.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hex File:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hex Data:";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(391, 415);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 11;
+            this.btnRead.Text = "Read!!!";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 445);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxHexFile);
+            this.Controls.Add(this.btn_burn);
+            this.Controls.Add(this.btnHexRead);
+            this.Controls.Add(this.rtbCodeView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxCom);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox cbxCom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbCodeView;
+        private System.Windows.Forms.Button btnHexRead;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_burn;
+        private System.Windows.Forms.TextBox tbxHexFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
---
+++ EEPROM_burner_vossy/EEPROM_burner_vossy/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.IO;
+using System.IO.Ports;
+
+namespace EEPROM_burner_vossy
+{
+    public partial class Form1 : Form
+    {
+        private Stream myStream = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHexRead_Click(object sender, EventArgs e)
+        {
+            rtbCodeView.Clear();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbxHexFile.Text = openFileDialog1.FileName;
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        for (int a = 0; a < myStream.Length; a++)
+                        {
+                            //.ToString("X") konvertiert eine zahl zu String(aber als hex)
+                            int b = myStream.ReadByte();
+                            if (b < 16) rtbCodeView.AppendText("0");
+
+                            rtbCodeView.AppendText(b.ToString("X") + " ");
+                        }
+                    }
+                    listBox1.Items.Add("File loaded ...");
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                }
+                catch (Exception ex)
+                {
+                    listBox1.Items.Add("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void btn_burn_Click(object sender, EventArgs e)
+        {
+            if (tbxHexFile.Text!="")
+            {
+                //Streamposition zurücksetzen damit vom ersten byte gelesen wird
+                myStream.Position = 0;
+                
+                //Byte 51 senden damit der Arduino weiß, dass geschrieben werden soll
+                byte cmd = 51;
+                byte[] b = BitConverter.GetBytes(cmd);
+                serialPort1.Write(b, 0, 1);
+
+                //Es sollen 8192 Byte gesendet werden
+                int laenge = 8192;               
+                
+                //die äussere schleife wird in 1024 gesendeten Byte intervallen aufgerufen
+                for (int a = 0; a < laenge/1024; a++)
+                {
+                    //Die innere schleife sendet 1024 Byte hintereinander
+                    for (int ii = 0; ii < 1024; ii++)
+                    {
+                        //sende byte wenn der Stream beendet ist, soll eine 0 gesendet werden
+                        if (myStream.Length <= a*1024+ii)
+                            cmd = Convert.ToByte(0);
+                        else
+                            cmd = Convert.ToByte(myStream.ReadByte());
+                        //Ausgeben was gesendet wurde
+                        listBox1.Items.Add("Sending Byte[" + (a*1024+ii) + "]: " + cmd.ToString("X"));
+                        byte[] bb = BitConverter.GetBytes(cmd);
+                        //senden
+                        serialPort1.Write(bb, 0, 1);
+                    }
+                    //Es wird gewartet, dass der Arduino fertig geschrieben hat und dies mitteilt
+                    serialPort1.Read(b,0,1);
+                }
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            // ein Byte mit "50" Senden damit der arduino weiß, dass gelesen werden soll
+            byte cmd = 50;
+            byte[] b = BitConverter.GetBytes(cmd);
+            serialPort1.Write(b, 0, 1);
+
+            //Es sollen 8192 Byte gelesen werden
+            long laenge = 8192;
+            
+            while (a<laenge)
+            {
+                try
+                {
+                    //empfangen der Bytes vom arduino
+                    serialPort1.Read(b, 0, 1);
+                }
+                catch (TimeoutException to)
+                {
+                    //userinfo falls der arduino nicht antwortet
+                    listBox1.Items.Add("Arduino not responding ... - CANCEL!");
+                    break;
+                }
+                //Byte in int convertieren damit es ausgegeben werden kann
+                int readByte = BitConverter.ToInt32(b, 0);
+
+                //Byte ausgeben
+                listBox1.Items.Add("Byte[" + (a) + "]: " + readByte.ToString("X"));
+                a++;
+            }
+        }
+
+        private void cbxCom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();        //todesangst unddo
+            }
+            catch { };
+            serialPort1.PortName = cbxCom.Text;
+            serialPort1.ReadTimeout = 1000000;
+            serialPort1.WriteTimeout = 100000;
+            serialPort1.Open();
+        }
+    }
+}
